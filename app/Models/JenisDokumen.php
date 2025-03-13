5f6d6a57b8d0c9146b689bdeeefe555d04427a4e
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-03-13 21:37:41 +0700

    `Refactor DokumenResource to use relationship with JenisDokumen model`

--- app/Models/JenisDokumen.php
+++ app/Models/JenisDokumen.php
@@ -5,14 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Dokumen extends Model
+class JenisDokumen extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
 
-    public function jenisDokumen()
+     public function dokumen()
     {
-        return $this->belongsTo(JenisDokumen::class, 'jenis_dokumen_id');
+        return $this->hasMany(Dokumen::class);
     }
 }
